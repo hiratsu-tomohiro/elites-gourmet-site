8193f1775b899808ebe2e4d59c11af714e687c1a
HIRATSU TOMOHIRO <hiratsu-tomohiro@av.avex.co.jp>
2017-01-19 03:01:45 +0000

    03_実習_レストランのCRUD作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   get 'restaurants/index', as: 'user_root'
   root 'restaurants#index'
+    resources :restaurants do
+      member do
+        patch 'confirm'
+      end
+      collection do
+        post 'confirm'
+      end
+    end
   
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
